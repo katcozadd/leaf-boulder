eb71038796b29ccfcb13f4f2ea6aa3348cd47add
katcozadd <krosecozadd@gmail.com>
2018-06-04 17:46:30 -0600

    working on a contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,1 +1,73 @@
-ContactForm.js
+import React, {Component} from 'react';
+import '../components/ContactForm.css';
+
+
+
+class ContactForm extends Component{
+  render() {
+    return (
+        <div >
+            <h3 id="contact">CONTACT</h3>
+            <p>Drop us a line...</p>
+            <section id="contact-form">
+                <form>
+                  <div className="row">
+                    <div className="six columns">
+                      <label htmlFor="firstNameInput">First Name
+                      <input 
+                      name = "firstName"
+                      className="u-full-width" 
+                      type="text" 
+                      placeholder="ex. John" 
+                      id="firstNameInput" />
+                      </label>
+                    </div>
+                    <div className="six columns">
+                      <label htmlFor="lastNameInput">Last Name
+                      <input 
+                      name = "lastName"
+                      className="u-full-width" 
+                      type="text" 
+                      placeholder="ex. Smith" 
+                      id="lastNameInput" />
+                      </label>
+                    </div>
+                    <div className="six columns">
+                      <label htmlFor="emailInput">Your email
+                      <input 
+                      name = "email"
+                      className="u-full-width" 
+                      type="email" 
+                      placeholder="ex. yourname@mailbox.com" 
+                      id="emailInput" />
+                      </label>
+                    </div>
+                    <div className="six columns">
+                      <label htmlFor="recipientInput">Subject
+                      <br />
+                      <input 
+                      name="subject"
+                      className="u-full-width" 
+                      type='text'
+                      placeholder='ex. Admiration'
+                      id="recipientInput" />
+                      </label>
+                    </div>
+                  </div>
+                  <label htmlFor="message">Message
+                  <textarea 
+                  name= "message"
+                  className="u-full-width" 
+                  placeholder="ex. Hi there, we visited your restaurant..." 
+                  id="exampleMessage">
+                  </textarea>
+                  <input  type="submit" value="Submit"/>
+                  </label>
+                </form>
+            </section>
+        </div>
+    );
+  }
+}
+
+export default ContactForm;
--- src/containers/ContactPageContainer.js
+++ src/containers/ContactPageContainer.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
+import ContactForm from '../components/ContactForm';
 
 
 class ContactPageContainer extends Component {
   render() {
     return (
       <div>
-      <h1>Contact Page</h1>
+      	<ContactForm />
       </div>
     );
   }
