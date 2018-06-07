681e94f4cd7897a26ae4aa22cbc775b77342ccfd
katcozadd <krosecozadd@gmail.com>
2018-06-06 20:52:00 -0600

    adding firebase to the form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,16 +1,139 @@
 import React, {Component} from 'react';
 import '../components/ContactForm.css';
+import fire from "../fire";
+import axios from 'axios'
 
 
 
 class ContactForm extends Component{
+  constructor(props) {
+      super(props);
+      this.state = {
+        contacts: {
+          data: []
+        },
+        newFirstName: "",
+        newLastName: "",
+        newEmail: "",
+        newSubject: "",
+        newMessage: ""
+
+      }
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+    this.onFirstNameChange = this.onFirstNameChange.bind(this)
+    this.onLastNameChange = this.onLastNameChange.bind(this)
+    this.onEmailChange = this.onEmailChange.bind(this)
+    this.onSubjectChange = this.onSubjectChange.bind(this)
+    this.onMessageChange = this.onMessageChange.bind(this)
+    };
+
+    componentWillMount() {
+      let contactRef = fire.database().ref('contacts').orderByKey().limitToLast(100);
+      contactRef.on('child_added', snapshot => {
+        let contact = { text: snapshot.val(), id: snapshot.key };
+        this.setState({ contacts: [contact].concat(this.state.contacts) });
+      })
+    }
+
+
+  onFormSubmit(event) {
+    event.preventDefault()
+    fire.database().ref('contacts').push({
+      FirstName : this.state.newFirstName,
+      LastName : this.state.newLastName,
+      Email : this.state.newEmail,
+      Subject : this.state.newSubject,
+      Message : this.state.newMessage
+    });
+    this.setState({
+      newFirstName: "",
+      newLastName: "",
+      newEmail: "",
+      newSubject: "",
+      newMessage: ""
+    });
+
+    let contactFormInfo = {
+      firstName: this.state.newFirstName,
+      lastName: this.state.newLastName,
+      email: this.state.newEmail,
+      subject: this.state.newSubject,
+      message: this.state.newMessage
+    }
+    axios.post('https://acafeboise.herokuapp.com/contact', contactFormInfo)
+    .then(response => { 
+      console.log(response)
+      this.props.history.push('/contact');
+})
+    .catch(error => {
+    console.log(error.response)
+    })
+  }
+
+  onFirstNameChange(event){
+    this.setState({
+      contacts : this.state.contact,
+      newFirstName : event.target.value,
+      newLastName : this.state.newLastName,
+      newEmail : this.state.newEmail,
+      newSubject : this.state.newSubject,
+      newMessage : this.state.newMessage
+    })
+
+  }
+
+    onLastNameChange(event){
+    this.setState({
+      contacts : this.state.contact,
+      newFirstName : this.state.newFirstName,
+      newLastName : event.target.value,
+      newEmail : this.state.newEmail,
+      newSubject : this.state.newSubject,
+      newMessage : this.state.newMessage
+    })
+
+  }
+
+  onEmailChange(event){
+    this.setState({
+      contacts : this.state.contact,
+      newFirstName : this.state.newFirstName,
+      newLastName : this.state.newLastName,
+      newEmail : event.target.value,
+      newSubject : this.state.newSubject,
+      newMessage : this.state.newMessage
+    })
+
+  }
+
+  onSubjectChange(event) {
+    this.setState({
+      contacts : this.state.contact,
+      newFirstName : this.state.newFirstName,
+      newLastName : this.state.newLastName,
+      newEmail : this.state.newEmail,
+      newSubject : event.target.value,
+      newMessage : this.state.newMessage
+    });
+  }
+
+  onMessageChange(event){
+    this.setState({
+      contacts : this.state.contact,
+      newFirstName : this.state.newFirstName,
+      newLastName : this.state.newLastName,
+      newEmail : this.state.newEmail,
+      newSubject : this.state.newSubject,
+      newMessage : event.target.value
+    })
+  }
   render() {
     return (
         <div >
             <h3 id="contact">CONTACT</h3>
             <p>Drop us a line...</p>
             <section id="contact-form">
-                <form>
+                <form onSubmit={this.onFormSubmit}>
                   <div className="row">
                     <div className="six columns">
                       <label htmlFor="firstNameInput">First Name
@@ -19,7 +142,9 @@ class ContactForm extends Component{
                       className="u-full-width" 
                       type="text" 
                       placeholder="ex. John" 
-                      id="firstNameInput" />
+                      id="firstNameInput"
+                      onChange={this.onFirstNameChange}
+                      value={this.state.newFirstName} />
                       </label>
                     </div>
                     <div className="six columns">
@@ -29,7 +154,9 @@ class ContactForm extends Component{
                       className="u-full-width" 
                       type="text" 
                       placeholder="ex. Smith" 
-                      id="lastNameInput" />
+                      id="lastNameInput"
+                      onChange={this.onLastNameChange}
+                      value={this.state.newLastName} />
                       </label>
                     </div>
                     <div >
@@ -39,7 +166,9 @@ class ContactForm extends Component{
                       className="u-full-width" 
                       type="email" 
                       placeholder="ex. yourname@mailbox.com" 
-                      id="emailInput" />
+                      id="emailInput"
+                      onChange={this.onEmailChange}  
+                      value={this.state.newEmail} />
                       </label>
                     </div>
                     <div >
@@ -50,7 +179,9 @@ class ContactForm extends Component{
                       className="u-full-width" 
                       type='text'
                       placeholder='ex. Admiration'
-                      id="recipientInput" />
+                      id="recipientInput"
+                      value={this.state.newSubject} 
+                      onChange={this.onSubjectChange} />
                       </label>
                     </div>
                   </div>
@@ -59,7 +190,9 @@ class ContactForm extends Component{
                   name= "message"
                   className="u-full-width" 
                   placeholder="ex. Hi there, we visited your restaurant..." 
-                  id="exampleMessage">
+                  id="exampleMessage"
+                  onChange={this.onMessageChange} 
+                  value={this.state.newMessage}>
                   </textarea>
                   </label>
                   <input id="button" type="submit" value="Submit"/>
---
+++ src/fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+  var config = {
+    apiKey: "AIzaSyBI8uIa9YXJK2OLH3fjadjnzFalfUR9pGE",
+    authDomain: "leaf-vegetarian-restaurant.firebaseapp.com",
+    databaseURL: "https://leaf-vegetarian-restaurant.firebaseio.com",
+    projectId: "leaf-vegetarian-restaurant",
+    storageBucket: "leaf-vegetarian-restaurant.appspot.com",
+    messagingSenderId: "821181775768"
+  };
+  let fire = firebase.initializeApp(config);
+  export default fire;
